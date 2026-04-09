ccf07049f3897715c645568fe099e3de6ac64c8c
levisrabah <levisrabah@gmail.com>
2026-04-10 02:06:10 +0300

    Update effects

--- effect.js
+++ effect.js
@@ -176,13 +176,13 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+			if(i==11){
+				// Keep the last message visible and don't fade it out
+				setTimeout(function() {
 					$('.cake').fadeIn('fast');
-				});
-				
+				}, 2000);
 			}
-			else{
+			else if(i<11){
 				msgLoop(i);
 			}			
 
